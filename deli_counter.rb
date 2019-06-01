5af27be5ff9147b1034089bab7520bfc3d42b0ab
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 12:27:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(arr)
   if arr.length < 1
     puts "The line is currently empty."
   else
+    result = 
     arr.each_with_index do |str, index|
     result += "#{index + 1}. {str}"
     end
