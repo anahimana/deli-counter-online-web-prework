8b4bffdfcb2637651ea4825b2502060471327028
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 13:03:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,8 +20,8 @@ def take_a_number(arr, arg)
     arr.push(arg)
     puts "Welcome, #{arr.last}. You are number #{arr.index(arr.last) + 1} in line."
   elsif arg.length > 1
-    arg.each do |item|
-    arr.push(item)
+      arg.each do |item|
+      arr.push(item)
     end
     # binding.pry
   end
