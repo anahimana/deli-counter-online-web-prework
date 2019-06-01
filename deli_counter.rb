efb095201bc7d7d6249e0752534be1a136fd9d97
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 12:54:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,8 @@ def line(arr)
 end
 
 def take_a_number(arr, arg)
-  if line(arr)
+  if arr.length < 1
+    arr.push(arg)
+    puts "Welcome, #{arr.last}. You are number #{arr.index(arr.last) + 1} in line."
+  end
 end
